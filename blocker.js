3a66098d6e200634102d5357f7a228ff14c23f4f
CandussoR <r.candusso@protonmail.com>
2024-04-25 18:16:02 +0200

    feat : adding basic structure of future checks for restrictions

--- blocker.js
+++ blocker.js
@@ -1,38 +1,66 @@
-// Is being run everytime a page is loaded.
+// I don't check onCreated since the url or pending are generally set through onUpdated.
 
-function checkIfAuthorized(sites) {
-    const site = sites.filter(x => x.site === window.location.hostname)[0]
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 
-    if (!site) {
-        return true;
+    let restrictedSites = await getRestrictedSites();
+    if (!restrictedSites) return ;
+
+    let url = changeInfo.pendingUrl || changeInfo.url
+    if (url === "chrome://newtab/") return;
+    console.log("pending or url", url)
+
+    if (url) {
+        let host = new URL(url).host
+            if (isRestricted(host, restrictedSites)) {
+                console.log("This host is restricted !")
+            }
+        }
+
+    if (changeInfo.audible === true) {
+        console.log("something started playing", tab.url)
+    } else if (changeInfo.audible === false) {
+        console.log("something stopped playing", tab.url)
     }
 
-    const day = new Date().getDay()
-    if (!site.restricted.days.includes(day)) {
-        return true;
-    }
+  });
 
-    const time = new Date().toLocaleTimeString('fr-FR')
-    for (let i = 0; i < site.restricted.time.length; i++) {
-        if (time < site.restricted.time[i][0] || time > site.restricted.time[i][1]) {
-            return false 
+async function getRestrictedSites() {
+    let {sites = []} = await chrome.storage.local.get('sites')
+    if (chrome.runtime.lastError) {
+        console.error("An error occurred while fetching your settings.")
+        return;
+    }
+    return sites
+}
+
+function isRestricted(site, sites) {
+    let sitesName = sites.map(x => x.site)
+    if (!sitesName.includes(site)) { return false; }
+
+    const currentDay = new Intl.DateTimeFormat("en-US", {"weekday" : "long"}).format(new Date)
+    const currentTime = new Date().toLocaleTimeString('fr-FR')
+    
+    let restrictions = sites[ sitesName.findIndex(x => x === site) ].restrictions
+    
+    if (restrictions.slots) { return checkSlots(restrictions.slots, currentDay, currentTime) }
+   
+    return false
+}
+
+function checkSlots(slots, currentDay, currentTime) {
+
+    for (let i=0 ; i < slots.length ; i++) {
+
+        if (slots[i].days.includes(currentDay)) {
+            console.log("includes day")
+            let spans = slots[i].time
+            for (let j = 0; j < spans.length; j++) {
+                if (currentTime > spans[j][0] && currentTime < spans[j][1]) {
+                    return true 
+                }
+            }
         }
     }
 
-    return true;
+    return false;
 }
-
-chrome.storage.local.get('sites', (res) => {
-    if (chrome.runtime.lastError) {
-        alert("An error occured while fetching your settings.")
-    }
-
-    if (Object.keys(res).length !== 0) {
-        sites = res.sites
-    }
-
-    sites = template_sites
-
-    if (checkIfAuthorized(sites) === false) alert("Unauthorized !")
-})
-
