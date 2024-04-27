9b0b74c16d45adcba3fb10650ef524527b74d27c
CandussoR <r.candusso@protonmail.com>
2024-04-27 17:07:52 +0200

    fix : edge case in blocker

--- blocker.js
+++ blocker.js
@@ -1,27 +1,18 @@
 // I don't check onCreated since the url or pending are generally set through onUpdated.
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-
     let restrictedSites = await getRestrictedSites();
     if (!restrictedSites) return ;
 
     let url = changeInfo.pendingUrl || changeInfo.url
-    if (url === "chrome://newtab/") return;
-    console.log("pending or url", url)
+    if (url === "chrome://newtab/" || url === "redirected/redirected.html") return;
 
     if (url) {
         let host = new URL(url).host
-            if (isRestricted(host, restrictedSites)) {
-                console.log("This host is restricted !")
-            }
+        if (isRestricted(host, restrictedSites)) {
+            chrome.tabs.update(tabId, {url : "redirected/redirected.html"}, () => {console.log(`redirected from ${host}`)})
         }
-
-    if (changeInfo.audible === true) {
-        console.log("something started playing", tab.url)
-    } else if (changeInfo.audible === false) {
-        console.log("something stopped playing", tab.url)
     }
-
   });
 
 async function getRestrictedSites() {
@@ -39,24 +30,27 @@ function isRestricted(site, sites) {
 
     const currentDay = new Intl.DateTimeFormat("en-US", {"weekday" : "long"}).format(new Date)
     const currentTime = new Date().toLocaleTimeString('fr-FR')
-    
-    let restrictions = sites[ sitesName.findIndex(x => x === site) ].restrictions
-    
+
+    let siteIndex = sitesName.findIndex(x => x === site)
+    let restrictions = sites[siteIndex].restrictions
+
     if (restrictions.slots) { return checkSlots(restrictions.slots, currentDay, currentTime) }
-   
+
     return false
 }
 
 function checkSlots(slots, currentDay, currentTime) {
-
     for (let i=0 ; i < slots.length ; i++) {
 
         if (slots[i].days.includes(currentDay)) {
-            console.log("includes day")
             let spans = slots[i].time
             for (let j = 0; j < spans.length; j++) {
-                if (currentTime > spans[j][0] && currentTime < spans[j][1]) {
-                    return true 
+                if (j === (spans.length - 1) && currentTime > spans[j][0] && spans[j][1] === '00:00:00') {
+                    return true
+                }
+                else if (currentTime > spans[j][0] && currentTime < spans[j][1]) {
+                    console.log("yes")
+                    return true
                 }
             }
         }
