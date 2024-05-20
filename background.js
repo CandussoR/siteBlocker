7bffc9a9fecc399609718655541b018fef791471
Romain Candusso <90688847+CandussoR@users.noreply.github.com>
2024-05-20 16:19:03 +0200

    feat : adding totalTime constraint
    * bugFix : no deletion on click.
    
    * feat : added records key to store totalTime at runtime in background, added handling of change of focus in tabs and window, added bookkeeping functions to count totalTime

--- background.js
+++ background.js
@@ -13,3 +13,41 @@ chrome.runtime.onInstalled.addListener(async () => {
     }
 })
 
+chrome.runtime.onStartup.addListener(async () => {
+    let date = new Date()
+    let todate = date.toISOString().split('T')[0] ;
+
+    let { records = {} } = await chrome.storage.local.get('records') ;
+    let { sites = [] } = await chrome.storage.local.get('sites') ;
+    let {cleanRecords: lastCleaned = []} = await chrome.storage.local.get('lastCleaned') ;
+    if (records.length === 0 && sites.length === 0) return ;
+    
+    records[todate] = {}
+    for (let i=0 ; i<sites.length ; i++) {
+        records[todate][sites[i].name] = {initDate : null, totalTime : 0, audible : false, tabId : null, focused : false } ;
+    }
+
+    if (!(lastCleaned.length === 0 || (lastCleaned[0] === todate)) ) {
+        records = cleanRecords(lastCleaned[0], records, todate) ;
+        await chrome.storage.local.set({lastCleaned : todate}) ;
+    }
+
+    await chrome.storage.local.set({records : records}) ;
+}) 
+
+function cleanRecords(lastCleaned, records, todate) {
+    let keys = Object.keys(records) ;
+    
+    let lastCleanIndex = keys.indexOf(lastCleaned) === -1 ? 0 : keys.indexOf(lastCleaned) ;
+    
+    for (let i= lastCleanIndex ; i < keys.length -1 ; i++) {
+        if (keys[i] >= todate) continue ;
+        let curr = records[keys[i]] ;
+        let props = Object.keys(curr) ;
+        for (let j=0 ; j < props.length ; j++) {
+            if (curr[props[j]].length !== 0) { curr[props[j]] = curr[props[j]].totalTime } ;
+        }
+    }
+
+    return records ;
+}
