3096f7e413d77301592ee2b783dfa7a898423850
Romain Candusso <r.candusso@protonmail.com>
2025-06-25 09:32:51 +0200

    feat : adding an automatic check for queues
    This allows the queue to automatically try to empty themselves if a condition prevents it at one time (already busy making changes in storage, mainly).

--- worker/blocker.js
+++ worker/blocker.js
@@ -6,8 +6,10 @@ export async function processOrEnqueue(flag, tabId = undefined, host = undefined
   let {busy} = await chrome.storage.local.get('busy')
 
   if (!busy && (bookkeepingQueue.queue.length > 0 || bookkeepingQueue.lastEvent === null)) {
-    bookkeepingQueue.dequeue({ flag : flag, tabId : tabId, host : host})
+    bookkeepingQueue.dequeue()
     return;
+  } else {
+    setTimeout(() => bookkeepingQueue.dequeue(), 1500)
   }
 }
 
--- worker/bookkeepingQueue.js
+++ worker/bookkeepingQueue.js
@@ -1,4 +1,5 @@
 import { bookkeeping } from "./bookkeeping.js";
+import { logger } from "./logger.js";
 
 class BookkeepingQueue {
     constructor() {
@@ -9,15 +10,29 @@ class BookkeepingQueue {
     addToQueue(bookkeepingParams) {
         if (this.lastEvent === 'no-focus' && bookkeepingParams.flag === 'no-focus') return;
         this.queue.push(bookkeepingParams);
+        // logger.debug("this is now queue", this.queue)
     }
 
     async dequeue() {
         if (this.queue.length === 0) return;
 
+        let {busy} = await chrome.storage.local.get("busy")
+        if (busy) {
+            // logger.debug("setting a TimeOut for dequeue")
+            setTimeout(() => {
+                logger.debug("TimeOut for dequeue activated")
+                this.dequeue()
+            }, 1500)
+            return;
+        }
+
         await chrome.storage.local.set({busy : true});
 
         while (this.queue.length !== 0) {
+            logger.debug("DEQUEUING ALL EVENTS")
             let {flag, tabId, host} = this.queue.shift();
+            logger.debug(`dequeuing ${flag} for tabId ${tabId} with host ${host}`, 
+                `last event is ${this.lastEvent}`)
             this.lastEvent = flag;
             if (this.lastEvent === "no-focus" && flag === "no-focus") continue;
             await bookkeeping(flag, tabId, host)
