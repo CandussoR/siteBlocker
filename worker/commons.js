0daca97616f3bbf49d9ae56ab54b210a0722e38d
Romain Candusso <r.candusso@protonmail.com>
2025-06-25 08:34:01 +0200

    feat : findAllTodayRestrictionsFor a site or group

--- worker/commons.js
+++ worker/commons.js
@@ -1,8 +1,35 @@
+
 export function findTodayRestriction(currentDay, restrictions, restrictionKey) {
     if (!restrictions || !(restrictionKey in restrictions)) {
         return undefined
     }
     return restrictions[restrictionKey].find(x => x.days.includes(currentDay))
+}
+
+/**
+ * 
+ * @param {Object} groupOrSite - the object group or object site to search
+ * @returns {Object|undefined} - the Restriction object filtered or undefined
+ */
+export function findAllTodayRestrictionsFor(groupOrSite) {
+      let currentDay = new Intl.DateTimeFormat("en-US", {
+        weekday: "long",
+      }).format(new Date());
+
+      if (!groupOrSite.restrictions) {
+        console.debug("No restrictions propriety on groupOrSite");
+        return undefined;
+      }
+
+      let ret = groupOrSite.restrictions;
+      for (let rest of Object.keys(ret)) {
+        ret[rest] = ret[rest].filter((x) => x.days.includes(currentDay));
+        if (!ret[rest].length) {
+          delete ret[rest];
+        }
+      }
+
+      return Object.keys(ret).length === 0 ? undefined : ret;
 }
 
 
