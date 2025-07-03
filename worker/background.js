725f711b8883f28f5cd9a32a3aa29445f0f6faf0
Romain Candusso <r.candusso@protonmail.com>
2025-07-03 15:18:29 +0200

    refactor : tabs.onUpdated

--- worker/background.js
+++ worker/background.js
@@ -8,6 +8,7 @@ import "./bookkeepingQueue.js";
 import {logger} from './logger.js';
 import { processOrEnqueue } from "./blocker.js";
 import { handleAlarms } from "./alarmsHandler.js";
+import { AlarmManager } from "./alarmManager.js";
 
 chrome.runtime.onInstalled.addListener(async () => {
   let { sites } = await chrome.storage.local.get("sites");
@@ -27,6 +28,7 @@ chrome.runtime.onInstalled.addListener(async () => {
 
 chrome.runtime.onStartup.addListener(async () => {
   await chrome.storage.local.set({ busy: true });
+
   let today = new Date().toISOString().split("T")[0];
   let records = await setRecords(today);
   if (!records) {
@@ -49,97 +51,78 @@ import { getSites, isRestricted } from "./restrictionsHandler.js";
 // Fires when the active tab in a window changes.
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
   let tab = await chrome.tabs.get(activeInfo.tabId);
-  console.log("changes on this tab", activeInfo);
   if (!tab.url) return;
-  let restrictedSites = await getSites();
+
+  logger.debug("onActivated : changes on this tab", activeInfo);
   let host = new URL(tab.url).host;
-  if (!restrictedSites.map((x) => x.name).includes(host)) {
+  if (!isInWatchedList(await getSites(), host)) {
     await processOrEnqueue("no-focus");
     return;
   }
+
   await processOrEnqueue("change-focus", activeInfo.tabId, host);
 });
 
 // Fires if window is focused or not
 chrome.windows.onFocusChanged.addListener(async (windowId) => {
   let [tab] = await chrome.tabs.query({ active: true });
-  console.log(
+  logger.debug(
     "onFocusChanged refocused window on following tab",
-    tab,
-    tab.tabId,
-    !tab.tabId
+    tab, tab.id
   );
 
-  if (windowId === -1 || !tab.tabId) {
+  if (windowId === -1 || !tab.id) {
     await processOrEnqueue("no-focus");
   } else {
-    let restrictedSites = await getSites();
-    if (
-      !tab.url ||
-      ["chrome://newtab/", "ui/redirected/redirected.html"].includes(tab.url)
-    )
-      return;
+    let sites = await getSites();
+    if ( !tab.url || isAppPageOrNewTab(tab.url)) return;
 
     let host = new URL(tab.url).host;
-    if (!restrictedSites.map((x) => x.name).includes(host)) return;
-    await processOrEnqueue("change-focus", tab.tabId, host);
+    if (!isInWatchedList(sites, host)) return;
+    await processOrEnqueue("change-focus", tab.id, host);
   }
 });
 
 // I don't check onCreated since the url or pending are generally set through onUpdated.
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  let changeUrl = changeInfo.pendingUrl || changeInfo.url;
-  let changeAudible = "audible" in changeInfo;
-  if (!changeUrl && !changeAudible) {
+  let treatUrl = shouldTreatUrl(changeInfo, tab);
+  if (!treatUrl && !("audible" in changeInfo))
     return;
-  }
-  if (
-    changeUrl.includes("chrome://newtab/") || changeUrl.includes("ui/")
-  ) {
+
+  if (isAppPageOrNewTab(changeUrl)) {
     await processOrEnqueue("no-focus");
     return;
   }
 
   if (tab.incognito) {
     let { restrictPrivate } = await chrome.storage.local.get("restrictPrivate");
-    if (!restrictPrivate) return;
+    if (!restrictPrivate) 
+      return;
   }
 
-  let flag = "open";
-  let restrictedSites = await getSites();
-  if (!restrictedSites) return;
+  let sites = await getSites();
+  if (!sites) 
+    return;
 
   let url = changeUrl || tab.url;
-  if (!url) return;
-
   let host = tab.incognito ? "private" : new URL(url).host;
-  if (!restrictedSites.map((x) => x.name).includes(host)) return;
+  if (!isInWatchedList(sites, host)) return;
 
-  if (await isRestricted(host, restrictedSites)) {
-    logger.debug("checked if isRestricted, processing close and redirecting")
-    await processOrEnqueue("close", tabId, host);
-
-    logger.info(`chrome.tabs.onUpdated : should redirect tab with url ui/redirected/redirected.html?url=${url}&host=${host}`)
-    logger.warning("Redirecting but not updating site record tab id")
-    chrome.tabs.update(tabId,
-      { url: `ui/redirected/redirected.html?url=${url}&host=${host}` }
-    );
+  const am = new AlarmManager(await chrome.alarms.getAll())
+  if (am.getEndAlarms(host) || await isRestricted(host, sites)) {
+    sendCloseAndRedirect(tabId, host, url);
     return;
   }
 
-  if (changeInfo.audible === true) {
-    flag = "audible-start";
-  } else if (changeInfo.audible === false) {
-    flag = "audible-end";
-  }
+  let flag = "open";
+  if ("audible in changeInfo")
+    flag = changeInfo.audible ? "audible-start" : "audible-end";
 
   logger.warning(`processOrEnqueuing ${flag} for tabId ${tabId} with host ${host}`)
   await processOrEnqueue(flag, tabId, host);
 });
 
-chrome.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
-  console.log("tab has been removed");
-  console.log(tabId, removeInfo);
+chrome.tabs.onRemoved.addListener(async (tabId, _) => {
   await processOrEnqueue("close", tabId);
 });
 
@@ -164,3 +147,21 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
     await chrome.alarms.clear(alarm.name)
   }
 });
+
+
+function shouldTreatUrl(changeInfo, tab) {
+  return changeInfo.pendingUrl || changeInfo.url || tab.url;
+}
+function isAppPageOrNewTab(url) {
+  return url.includes("chrome://newtab/") || url.includes("ui/")
+}
+function isInWatchedList(sites, host) {
+  return sites.map((x) => x.name).includes(host)
+}
+async function sendCloseAndRedirect(tabId, host, url) {
+    logger.debug("sendCloseAndRedirect : checked if isRestricted, processing close and redirecting")
+    await processOrEnqueue("close", tabId, host);
+    chrome.tabs.update(tabId,
+      { url: `ui/redirected/redirected.html?url=${url}&host=${host}` }
+    );
+  }
