e09cbc42163253b349cc3f4985edb448de8d6e13
Romain Candusso <r.candusso@protonmail.com>
2025-07-04 18:19:11 +0200

    refactor : made a guard out of condition (no-focus after no-focus)

--- worker/blocker.js
+++ worker/blocker.js
@@ -1,6 +1,9 @@
 import { bookkeepingQueue } from './bookkeepingQueue.js'
 
 export async function processOrEnqueue(flag, tabId = undefined, host = undefined) {
+  if (bookkeepingQueue.lastEvent === "no-focus" && flag === "no-focus")
+    return ;
+  
   bookkeepingQueue.addToQueue({ flag : flag, tabId : tabId, host : host})
 
   let {busy} = await chrome.storage.local.get('busy')
