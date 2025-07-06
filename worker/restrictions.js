79d1f4e41dd35d8aad6549d83799623276540a55
Romain Candusso <r.candusso@protonmail.com>
2025-07-06 08:52:36 +0200

    tmp: refactor TotalTime constraint.

--- worker/restrictions.js
+++ worker/restrictions.js
@@ -1,3 +1,9 @@
+/**
+ * @typedef {Object} ViolationStatus - Object that restrictions return for an AlarmManager
+ * @property {boolean} violated - if restriction is violated or not
+ * @property {Number} minutesBeforeRes - Minutes calculated before restriction
+ */
+
 // Represents a collection of time slot restrictions for a given entity (e.g., site or group)
 export class TimeSlotRestriction {
   /**
@@ -66,5 +72,86 @@ export class TimeSlotRestriction {
     }
     
     return undefined;
+  }
+}
+
+import { RecordManager } from "./recordManager.js";
+import { EntitiesCache, Site } from "./siteAndGroupModels.js";
+
+/**
+ * Use Case :
+ * TotalTimeRestriction(Group, rm, ec)
+ */
+export class TotalTimeRestriction {
+  /**
+   * @typedef {Object} TotalTimeConfig
+   * @property {string[]} days - days in english where this restrictions happen
+   * @property {Number} totalTime - total time in seconds
+   */
+  /**
+   * @param {Site|Group} groupOrSiteObj - the site or group we want to evaluate
+   * @param {RecordManager} rm - the RecordManager singleton
+   * @param {EntitiesCache} ec - the EntitiesCache singleton
+   */
+  constructor(groupOrSiteObj, rm, ec) {
+    this.entity = groupOrSiteObj;
+    /** @type {TotalTimeConfig[]|undefined} */
+    this.restriction = this.entity.todayRestrictions?.totalTime
+    this.rm = rm;
+    this.ec = ec;
+  }
+
+  /**
+   * @param {Object} Metadata - what site or group is to be checked and how
+   * @param {string} metadata.target - a host or group name
+   * @param {'site'|'group'} metadata.type
+   * @returns {ViolationStatus}
+   */
+  isViolated() {
+    if (!this.restriction) {
+      return { violated : false, minutesBeforeRes : undefined }
+    }
+
+    let result = this.entity instanceof Site
+        ? this.#getSiteTimeLeft(this.entity.name)
+        : this.#getGroupTimeLeft(this.entity.sites);
+
+    if (this.entity instanceof Site && result > 0) {
+      let group = this.ec.getGroupByName(this.entity.group);
+      let groupRes = this.#getSiteGroupTimeLeft(group);
+      if (groupRes.violated && groupRes.minutesBeforeRes < result) {
+        return groupRes;
+        }
+    }
+
+    return result > 0
+      ? { violated: true, minutesBeforeRes: result }
+      : { violated: false, minutesBeforeRes: undefined };
+  }
+
+  /**
+   * 
+   * @param {Group} group - the corresponding Group to a site
+   * @returns {ViolationStatus} - if the group violates a totalTime restriction or not
+   */
+  #getSiteGroupTimeLeft(group) {
+    return new TotalTimeRestriction(group, this.rm, this.ec).isViolated();
+  }
+
+  #getGroupTimeLeft(targets) {
+    return targets.reduce( (sum, t) => this.#getSiteTimeLeft(t) + sum, 0);
+  }
+
+  #getSiteTimeLeft(target) {
+    let tsr = this.rm.getTodaySiteRecord(target);
+    return this.#getTimeBeforeRestriction(tsr);
+  }
+
+  #getTimeBeforeRestriction(siteRec) {
+    if (!siteRec.initDate)
+      return (this.restriction.totalTime - siteRec.totalTime) / 60;
+
+    let tt = siteRec.totalTime + (new Date() - siteRec.initDate) / 1000;
+    return (this.restriction.totalTime - tt) / 60;
   }
 }
